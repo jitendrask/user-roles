4a9e476a53a74bc2d011bc79ca617530900f88bb
Jitendra Singh Kushwaha <jitendra@Jitendras-MacBook-Pro.local>
2025-09-19 13:27:05 +0530

    security patch

--- src/main/java/com/example/demo/demo/configuration/SecurityConfig.java
+++ src/main/java/com/example/demo/demo/configuration/SecurityConfig.java
@@ -2,6 +2,7 @@ package com.example.demo.demo.configuration;
 
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
+import org.springframework.http.HttpMethod;
 import org.springframework.security.config.annotation.web.builders.HttpSecurity;
 import org.springframework.security.core.userdetails.User;
 import org.springframework.security.core.userdetails.UserDetailsService;
@@ -17,8 +18,8 @@ public class SecurityConfig {
     public SecurityFilterChain filterChain(HttpSecurity http) throws Exception {
         http.csrf(csrf -> csrf.disable())
                 .authorizeHttpRequests(auth -> auth
-                        .requestMatchers("/users").hasAnyRole("ADMIN", "VIEWER") // GET allowed
-                        .requestMatchers("/users/**").hasRole("ADMIN") // POST allowed
+                        .requestMatchers(HttpMethod.GET,"/users").hasAnyRole("ADMIN", "VIEWER") // GET allowed
+                        .requestMatchers(HttpMethod.POST,"/users/**").hasRole("ADMIN") // POST allowed
                         .anyRequest().authenticated()
                 )
                 .httpBasic(); // Basic Auth
--- src/main/java/com/example/demo/demo/controller/UserController.java
+++ src/main/java/com/example/demo/demo/controller/UserController.java
@@ -4,6 +4,7 @@ import com.example.demo.demo.entity.UserEntity;
 import com.example.demo.demo.model.UserDto;
 import com.example.demo.demo.repository.UserRepository;
 import com.example.demo.demo.service.UserService;
+import org.springframework.security.access.prepost.PreAuthorize;
 import org.springframework.web.bind.annotation.*;
 import java.util.List;
 
@@ -17,11 +18,13 @@ public class UserController {
         this.userService = userService;
     }
 
+    @PreAuthorize("hasRole('ADMIN')")
     @PostMapping
     public String addUser(@RequestBody UserDto user) {
         return userService.addUser(user);
     }
 
+    @PreAuthorize("hasAnyRole('ADMIN','VIEWER')")
     @GetMapping
     public List<UserDto> listUsers() {
         return userService.getAllUsers();
